44e8c4b7e092fe324878aab5c3fc2682bc25b9d8
MShoaibSadiq845 <shoaib_sadiq_intern@netixsol.com>
2026-08-19 09:28:32 -0700

    ok

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'http://localhost:3001',
+      'https://cryptoapp-two-henna.vercel.app'
     ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
